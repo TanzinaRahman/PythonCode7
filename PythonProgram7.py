f6aeb50e0d56a90699eed2ce4b7c687a655eecd9
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-08 21:01:50 +0600

    Create PythonProgram7.py

---
+++ PythonProgram7.py
@@ -0,0 +1,6 @@
+#Triangle ; area = 1/2 * b * h ; [b=base , h=vertical height]
+
+base = float(input("Enter base : "))
+height = float(input("Enetr height : "))
+area = 0.5 * base * height
+print(area)
